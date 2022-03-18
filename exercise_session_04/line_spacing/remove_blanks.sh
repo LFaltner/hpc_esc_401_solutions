b3fafc89ea755654a1557f7ddbd4d11673baad8f
Lup09 <lukas.faltner@aon.at>
2022-03-18 19:26:07 +0100

    line spacing bash script

---
+++ exercise_session_04/line_spacing/remove_blanks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+if [ -f "$1" ] 
+	then
+		sed '/^$/d' "$1"
+	else
+		echo "File does not exist."
+		exit 1
+fi
