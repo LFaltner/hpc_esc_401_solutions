e9d058f70f88ed901d55686b375bf11049eb789a
Lup09 <lukas.faltner@aon.at>
2022-03-30 23:02:08 +0200

    vector addition implemented

--- exercise_session_05/ex1/vector.c
+++ exercise_session_05/ex1/vector.c
@@ -6,9 +6,11 @@
 int N = 1000;
 
 struct complex_vector{
-	double real;
-	double im;
+	int real;
+	int im;
 };
+
+void linear_combination(struct complex_vector *x, struct complex_vector *y, int a);
 
 
 int main(int argc, char** argv){
@@ -17,10 +19,28 @@ int main(int argc, char** argv){
 	struct complex_vector *x = malloc(N * sizeof(struct complex_vector));
 	struct complex_vector *y = malloc(N * sizeof(struct complex_vector));
 
-	double *a = malloc(sizeof(double));
+	int a = 10;
 
-	// filling the vectors with random values
+	// filling the vectors with random values in range [1,100]
+	srand(1);
+	int i;
+	for (i = 0; i < N; i++) {
+    x[i].real = rand() % 100 + 1;
+    x[i].im = rand() % 100 + 1;
+    y[i].real = rand() % 100 + 1;
+    y[i].im = rand() % 100 + 1;
+}
 
-	// define function for calculation
 
+	// use linear_combination function for calculation
+	linear_combination(x , y, a);
+	return 0;
+}
+
+void linear_combination(struct complex_vector *x, struct complex_vector *y, int a){
+	struct complex_vector z;
+	z.real = x->real + a * y->real;
+	z.im = x->im + a * x->im;
+
+	printf("z = %d + %di\n", z.real, z.im);
 }
