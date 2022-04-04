acb2aea1c095e1bcb7925268f52d3e610227a53c
Lup09 <lukas.faltner@aon.at>
2022-04-04 19:12:49 +0200

    OpenMP version

--- exercise_session_06/poisson_OMP/jacobi.cpp
+++ exercise_session_06/poisson_OMP/jacobi.cpp
@@ -1,5 +1,6 @@
 #include "jacobi.h"
 #include <math.h>
+#include <omp.h>
 
 /**
  * @brief      Computes norm of the difference between two matrices
@@ -12,8 +13,13 @@
  */
 double norm_diff(params p, double** mat1, double** mat2){
     double sum = 0.0;
+
+    #pragma omp parallel for
     for (int i=0; i<p.nx; i++){
+
+    #pragma omp parallel for
     for (int j=0; j<p.ny; j++){
+        #pragma omp atomic
         sum += (mat1[i][j] - mat2[i][j])*(mat1[i][j] - mat2[i][j]);
     }
     }
@@ -41,8 +47,12 @@ void jacobi_step(params p, double** u_new, double** u_old, double** f){
             u_old[i][j] = u_new[i][j];
     }
 
+    #pragma omp parallel for
     for (int i=1; i<p.nx-1; i++){
+
+        #pragma omp parallel for
         for (int j=1; j<p.ny-1; j++)
+            #pragma omp atomic
             u_new[i][j] = 0.25*(u_old[i-1][j] + u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1] - dx*dy*f[i][j]);
     }
     // printf("Function jacobi_step (jacobi.cpp l.26): not implemented.\n");
